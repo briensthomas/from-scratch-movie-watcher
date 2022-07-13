438688d7b793b54bd5219eade57d0e92f04cba7b
Brien Thomas <thomasbrien94@gmail.com>
2022-07-12 17:09:37 -0700

    added environment keys

---
+++ src/services/fetch-utils.js
@@ -0,0 +1,13 @@
+import { client } from './client.js';
+
+export async function signUpUser(email, password) {
+  const response = await client.auth.signUp({ email, password });
+
+  return response.user;
+}
+
+export async function signInUser(email, password) {
+  const response = await client.auth.signIn({ email, password });
+
+  return response.user;
+}
